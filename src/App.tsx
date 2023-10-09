162df6ce7b45d610d460a1fc7134fad23a71758a
ikeawesom <ikelimjl@gmail.com>
2023-10-09 23:48:35 +0800

    added IR text area

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import DetailsArea from "./components/DetailsArea";
 import FormContainer from "./components/FormContainer";
 import Header from "./components/Header";
+import { useDetails } from "./contexts/DetailsContext";
+import { Toaster, toast } from "sonner";
 
 export default function App() {
+  const { details } = useDetails();
+
   return (
     <div className="p-4 bg-slate-50">
+      <Toaster richColors position="bottom-center" />
       <div className="flex flex-col gap-10 items-center justify-center p-10">
         <Header />
         <FormContainer />
+        {details.submit && (
+          <DetailsArea onCopy={() => toast.success("Copied to clipboard!")} />
+        )}
       </div>
       <p className="text-center text-slate-400">Created by Ike Lim.</p>
     </div>
---
+++ src/components/DetailsArea.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useDetails } from "../contexts/DetailsContext";
+import handleFormat from "../handleFormat";
+import Clipboard from "./Clipboard";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+type DetailsType = {
+  onCopy: () => void;
+};
+
+const PLATFORMS = [
+  { title: "WhatsApp", src: "whatsapp.svg", id: "ws" },
+  { title: "Telegram", src: "telegram.svg", id: "tele" },
+];
+
+export default function DetailsArea({ onCopy }: DetailsType) {
+  const { details, setDetails } = useDetails();
+  const [formated, setFormatted] = useState("");
+
+  useEffect(() => {
+    const data = handleFormat(details, details.platform);
+    setFormatted(data);
+  }, [details]);
+
+  return (
+    <div className="sm:w-[580px] w-[80vw] flex flex-col gap-6">
+      <h4 className="text-base text-slate-800 text-center">
+        Your{" "}
+        <span className="text-violet-600 font-medium">incident report</span> has
+        been generated. Simply copy and paste this message below and send it
+        straight!
+      </h4>
+
+      <ul className="w-full flex items-center justify-center gap-x-10 gap-y-4 flex-wrap">
+        {PLATFORMS.map((item, index) => (
+          <li
+            onClick={() => {
+              setDetails({ ...details, platform: item.id });
+            }}
+            key={index}
+            className={`flex flex-row items-center justify-between gap-2 px-4 py-2 rounded-md shadow-md bg-white  duration-200 border-[1px] ${
+              details.platform === item.id
+                ? "border-violet-600 cursor-default"
+                : "cursor-pointer hover:brightness-95"
+            }`}
+          >
+            <img src={item.src} alt="" width={25} />
+            <h4>{item.title}</h4>
+          </li>
+        ))}
+      </ul>
+
+      <div className="relative">
+        <textarea
+          className="bg-white rounded-lg shadow-md resize-none w-full md:p-6 p-4 h-[75vh] details-area"
+          defaultValue={formated}
+          readOnly
+        />
+        <CopyToClipboard text={formated} onCopy={onCopy}>
+          <div className="absolute bottom-5 right-5 shadow-md rounded-full bg-violet-600 p-4 cursor-pointer hover:opacity-80 duration-200">
+            <Clipboard />
+          </div>
+        </CopyToClipboard>
+      </div>
+    </div>
+  );
+}
