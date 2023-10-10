1c132c38f096d282e12679b78c71df3fb35ba946
ikeawesom <ikelimjl@gmail.com>
2023-10-10 14:05:16 +0800

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-3 items-center justify-center text-center">
+      <img src="favicon.svg" alt="icon" width={40} />
       <h1 className="text-3xl font-bold text-slate-900">
         Welcome to <span className="text-violet-600">IR Generator</span>
       </h1>
