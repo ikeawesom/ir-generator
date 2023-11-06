2eb53fb7ef4acda15c9d75107a0fbbed649484b1
ikeawesom <ikelimjl@gmail.com>
2023-11-06 19:20:16 +0800

    conditional form

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,10 +1,11 @@
 import DefaultForm from "./Forms/DefaultForm";
+import TekongForm from "./Forms/TekongForm";
 
 export default function FormContainer({ option }: { option: string }) {
   return (
     <div className="bg-white md:p-8 sm:p-6 p-4 rounded-lg shadow-md sm:w-[580px] w-[85vw]">
       <p className="text-red-500 text-sm text-start mb-4">*Required fields</p>
-      {option === "keat-hong-camp" ? <DefaultForm /> : ""}
+      {option === "keat-hong-camp" ? <DefaultForm /> : <TekongForm />}
     </div>
   );
 }
