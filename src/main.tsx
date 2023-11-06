aca70ba8d5cb319362b53033cd775dce5e2ae3eb
ikeawesom <ikelimjl@gmail.com>
2023-11-06 10:12:33 +0800

    added browser router for navigation

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { DetailsProvider } from "./contexts/DetailsContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DetailsProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </DetailsProvider>
   </React.StrictMode>
 );
