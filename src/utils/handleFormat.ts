866c82ace7062d5a19ec7f6696283bd40cff0ecd
ikeawesom <ikelimjl@gmail.com>
2023-12-03 15:26:02 +0800

    modified formatting

--- src/utils/handleFormat.ts
+++ src/utils/handleFormat.ts
@@ -14,33 +14,32 @@ export default function handleFormat(details: DetailsType, type: string) {
 
   const dateTimeHeader = `${bold}2) Date & Time of Incident:${bold}`;
 
-  const svcmenHeader = `${bold}3) Serviceman Involved:${bold}`;
+  const svcmenHeader = `${bold}3) Particulars of Individual:${bold}`;
   const nric = `${details.nric}`;
   const name = `${details.name}`;
   const svs = `${details.svs}`;
   const pes = `${details.pes}`;
   const svcmen = `${name}, ${nric}, PES ${pes}, ${svs}`;
 
-  const unitInvolvedHeader = `${bold}4) Serviceman Unit and Company:${bold}`;
+  const locationHeader = `${bold}4) Location of Incident:${bold}`;
 
-  const locationHeader = `${bold}5) Location of Incident:${bold}`;
+  const descHeader = `${bold}5) Brief Description of Incident:${bold}`;
 
-  const descHeader = `${bold}6) Brief Description of Incident:${bold}`;
-
-  const injuryHeader = `${bold}7) Injury/Damage:${bold}`;
-
-  const actionHeader = `${bold}8) Follow Up Actions:${bold}`;
+  const statusHeader = `${bold}6) Current Status:${bold}`;
 
   const statuses = handleOptionsFormat(details.status);
   const emptyStatuses = statuses === "NIL";
 
-  const gsocHeader = `${bold}9a) Date/Time of Verbal Report to GSOC:${bold}`;
-  const esisHeader = `${bold}9b) Date/Time of ESIS Report:${bold}`;
-  const armHeader = `${bold}10) Date/Time reported to 3 DIV/HQ Armour:${bold}`;
+  const NOKHeader = `${bold}7) NOK Informed:${bold}`;
 
-  const roHeader = `${bold}11) Reporting Officer:${bold}`;
+  const HHQHeader = `${bold}8) HHQ (GSOC/Div/Bde) Informed:${bold}`;
+  const gsocHeader = `${bold}- GSOC:${bold}`;
+  const esisHeader = `${bold}- HQ 3 Div:${bold}`;
+  const armHeader = `${bold}- Bde:${bold}`;
 
-  const result = `${bold}${details.unit}${bold}
+  const roHeader = `${bold}Reporting By:${bold}`;
+
+  const result = `${bold}${details.unit}/${details.involved}${bold}
   
 ${natureHeader}
 ${details.nature}
@@ -51,25 +50,21 @@ ${details.idate} ${details.itime}HRS
 ${svcmenHeader}
 ${svcmen}
 
-${unitInvolvedHeader}
-${details.unit} ${details.involved}
-
 ${locationHeader}
 ${details.ilocation}
 
 ${descHeader}
 ${details.desc}
 
-${injuryHeader}
-${details.injury}
+${statusHeader}
+${emptyStatuses ? "NIL" : `${statuses}`}
 
-${actionHeader}
-${emptyStatuses ? details.actions : details.actions + `\n${statuses}`}
+${NOKHeader}
+${details.nok} on ${details.nokdate}HRS
 
+${HHQHeader}
 ${gsocHeader} ${details.gsoc === "" ? "NIL" : `${details.gsoc}HRS`}
-
 ${esisHeader} ${details.esis === "" ? "NIL" : `${details.esis}HRS`}
-
 ${armHeader} ${details.arm === "" ? "NIL" : `${details.arm}HRS`}
 
 ${roHeader}
