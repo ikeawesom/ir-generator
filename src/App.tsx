6dada719115f92396b45cd826232bc1dd1fde958
ikeawesom <ikelimjl@gmail.com>
2024-01-02 00:46:13 +0800

    added options to manage state

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import { SecondaryButton } from "./components/Buttons";
 import { useDetails } from "./contexts/DetailsContext";
 import { Toaster, toast } from "sonner";
 import { useSearchParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTekongDetails } from "./contexts/TekongContext";
 
 const OPTIONS = ["tekong", "keat-hong-camp"];
 
 export default function App() {
+  const [curState, setCurState] = useState<"initial" | "follow-up" | "final">(
+    "initial"
+  );
+
   const { details } = useDetails();
   const { tekongDetails } = useTekongDetails();
   let [searchParams, setSearchParams] = useSearchParams();
@@ -20,6 +24,10 @@ export default function App() {
     if (!option || !OPTIONS.includes(option))
       setSearchParams({ option: "tekong" });
   }, [searchParams, setSearchParams]);
+
+  useEffect(() => {
+    localStorage.setItem("state", curState);
+  }, [curState]);
 
   const paramTekong = () => {
     setSearchParams({ option: "tekong" });
@@ -72,6 +80,49 @@ export default function App() {
               </SecondaryButton>
             </div>
           </div>
+          {option === "tekong" && (
+            <div className="sm:w-[580px] w-[85vw] flex flex-col items-center justify-center gap-2">
+              <p className="text-center text-slate-400">
+                Select the <span className="font-bold">current</span> state of
+                the IR.
+              </p>
+              <div className="flex w-full items-center justify-center gap-x-4 gap-y-2 max-[430px]:flex-wrap">
+                <SecondaryButton
+                  disabled={curState === "initial"}
+                  className={
+                    curState === "initial"
+                      ? "cursor-default bg-violet-600 text-slate-50"
+                      : ""
+                  }
+                  onClick={() => setCurState("initial")}
+                >
+                  Initial
+                </SecondaryButton>
+                <SecondaryButton
+                  disabled={curState === "follow-up"}
+                  className={
+                    curState === "follow-up"
+                      ? "cursor-default bg-violet-600 text-slate-50"
+                      : ""
+                  }
+                  onClick={() => setCurState("follow-up")}
+                >
+                  Follow Up
+                </SecondaryButton>
+                <SecondaryButton
+                  disabled={curState === "final"}
+                  className={
+                    curState === "final"
+                      ? "cursor-default bg-violet-600 text-slate-50"
+                      : ""
+                  }
+                  onClick={() => setCurState("final")}
+                >
+                  Final
+                </SecondaryButton>
+              </div>
+            </div>
+          )}
           <FormContainer option={option} />
         </div>
         {submitted && (
